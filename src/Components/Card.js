437e511d372b93a1f5c49026ad788283f930268b
Agnes Dormal <agnesdormal@hotmail.com>
2021-06-21 20:30:22 -0300

    -

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -23,7 +23,7 @@ export default class Card extends Component {
                 <Image style={card.image} source={{uri: this.props.image}}></Image>
                 <View style={card.item}><Text>Nombre: {this.props.name} {this.props.lastname}</Text></View> 
                 <View style={card.item}><Text>Email: {this.props.email}</Text></View>
-                <View style={card.item}><Text>{this.state.birthdate} {this.props.age} años</Text></View>
+                <View style={card.item}><Text>{this.state.birthdate} - {this.props.age} años</Text></View>
                 <View style={card.item}><Text>Tel: {this.props.phone} </Text></View>
                 <View style={card.item}><Text>Direccion: {this.props.number} {this.props.address}</Text></View>
                 <View style={card.item}><Text>Registrado:  {this.state.registro} </Text></View>

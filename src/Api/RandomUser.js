4d942cc2dc9729dd97ee606e1e6919bb5a8270b7
Agnes Dormal <agnesdormal@hotmail.com>
2021-06-21 15:09:57 -0300

    API y tarjetas

--- src/Api/RandomUser.js
+++ src/Api/RandomUser.js
@@ -1,18 +1,10 @@
- /*export async function getData(){
-     Ahora definimos la función con async debido a que vamos a realizar un llamado asincrónico 
+export async function getData(q) {
     try {
-        let resultado= await fetch ('https://randomuser.me/api?results=20');
-         La instruccion await va a esperar que la promesa (respuesta) se resuelva (lo procesa )y 
-        cuando se resuelva la  lo va a guardar en la variable 
-        let json = await resultado.json ()
-         El json devuelve una promesa, entonces antepongo el await y luego el resultado de la promesa va
-         a ser guardado en un json. Cuando lo obtengo ya puedo devolver el valor sin inconvenientes.
-return json.results; 
-}catch (error) {
-Console.error(error);
+        let resultado = await fetch("https://randomuser.me/api/?results=" + q);
+        let json = await resultado.json();
+        console.log(json)
+        return json.results;
+    } catch(e) {
+        console.log(e);
+    }
 }
-};
-
-  
- con el async es para almacenar en el dispositivo no es para hacer un fetch en si 
- */

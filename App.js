37113055d1d989da974dd875ed3a38f4d19b0113
Agnes Dormal <agnesdormal@hotmail.com>
2021-06-21 15:17:37 -0300

    screen uno kruger te borre cosas

--- App.js
+++ App.js
@@ -1,64 +1,37 @@
-import "react-native-gesture-handler";
-import React, {Component} from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import { 
-    View,
-    Text,
-    ScrollView,
-    StyleSheet,
-    TextInput,
-    TouchableOpacity,
-     }
-    from 'react-native';
-
-import  Screen_1 from "./Screens/Screen_1";
-import  Screen_2 from "./Screens/Screen_2";
-import  Screen_3 from "./Screens/Screen_3";
-import Screen_ViewCards from "./Screens/Screen_ViewCards"
-import Screen_Import from "./Screens/Screen_Import"
+import React, { Component } from 'react';
+import { styles } from './src/styles/Styles';
+import Header from './src/Components/Header';
+import Screen from './src/screens/Screen_2';
+import Footer from './src/components/Footer';
 
 
+import { View } from "react-native";
+
+export default class App extends Component {
+  
+	constructor() {
+		super();
+			this.state = {}
+		}  
 
 
-const Drawer = createDrawerNavigator ();
+		render (){
+			return (
+				<View style={styles.layout}>
 
-class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>     
-      <Drawer.Navigator initialRouteName= "Screen_Import"
-      edgeWidth={100}
-      drawerPosition = "left"
-      drawerType = "slide"
-      overlayColor= "#4444ff"
-      drawerStyle={{
-        backgroundColor:"#aaaa99",
-        width:250
+					<View style={styles.layout}>
+						<Header/>
+					</View>
 
-      }}
-      drawerContentOptions={{
-        activeTintColor:"white",
-        activeBackgroundColor:"blue",
-        inactiveTintColor:"black",
-        inactiveBackgroundColor:"orange",
-        itemStyle:{borderRadius:100, borderWidth:3},
-        
-      }}
-      >
-        <Drawer.Screen name="Screen 1" component= {Screen_1} options ={{title:"Menu"}}/>
-        <Drawer.Screen name="Screen View" component= {Screen_ViewCards} options ={{title:"Menu"}}/>
-        <Drawer.Screen name="Screen 2 " component= {Screen_2} options ={{title:"importar"}}/>
-        <Drawer.Screen name="Screen Import" component= {Screen_Import} options ={{title:"Contactos importados"}}/>
-        <Drawer.Screen name="Screen 3 " component= {Screen_3} initialParams = {{valor: 0}}/>
-      </Drawer.Navigator>
+					<View style={styles.layout}>
+						<Screen/>
+					</View>
 
-      </NavigationContainer>
-
-    )
+					<View style={styles.layout}>
+						<Footer/>
+					</View>
+				
+				</View>
+  			)
+		}  
   }
-}
-
-export default App
--- Styles/styles.js
+++ Styles/styles.js
@@ -1,42 +1,9 @@
-import  {StyleSheet} from "react-native";
- 
-export const styles = StyleSheet.create ({
-    container:{
-    flex:1,
-    justifyContent: "center",
-    alignItems: "center"
-    },
-    
-    title: {
-      fontSize:30,
-      color:"blue",
-      fontWeight: "bold",
+import { StyleSheet } from 'react-native';
 
-    
-    },
-    texto:{
-        fontSize:20,
-        color:"black",
-        fontWeight:"bold",
-    },
-    burguerButton:{
-     width:80,
-     height:80,
-     backgroundColor:"blue",
-     borderRadius:100,
-     alignItems:"center",
-     justifyContent:"center",
-
-    },
-    burguerText:{
-        fontSize:30,
-        color :"white"
-    },
-    burguerContainer:{
-        position:"absolute",
-        top:10,
-        left:10
+const styles = StyleSheet.create({
+    layout: {
+        flex: 3
     }
+})
 
-  })
-  
+export { styles }
